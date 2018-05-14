1025d604d03c2de989559674115cda4d21c05bc5
Diego Ortín <dieortin@gmail.com>
2018-05-14 02:50:19 +0200

    Changed grantview into a more generic textview, which provides the ability to view additionalInfo from the participants as well. Added such ability to the approval view

---
+++ routes/textview.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const debugTextView = require('debug')('congressweb:textView')
+
+const Participant = require('../models/Participant')
+
+router.get('/grant/:id', (req, res) => {
+	req.app.locals.renderingOptions.title = 'Grant explanation'
+	Participant.findById(req.params.id, (err, participant) => {
+		if (err) {
+			debugTextView(`Error while attempting to view grant explanation ${req.params.id}`)
+			res.statusCode = 500
+			res.end()
+			return
+		}
+		debugTextView(`Showing grant explanation ${req.params.id}`)
+		res.app.locals.renderingOptions.name = participant.personalData.firstName
+		res.app.locals.renderingOptions.text = participant.grant.explanation
+		res.render('textview', res.app.locals.renderingOptions)
+	})
+})
+
+router.get('/additionalInfo/:id', (req, res) => {
+	req.app.locals.renderingOptions.title = 'Additional information'
+	Participant.findById(req.params.id, (err, participant) => {
+		if (err) {
+			debugTextView(`Error while attempting to view additional information for ${req.params.id}`)
+			res.statusCode = 500
+			res.end()
+			return
+		}
+		debugTextView(`Showing additional information ${req.params.id}`)
+		res.app.locals.renderingOptions.name = participant.personalData.firstName
+		res.app.locals.renderingOptions.text = participant.additionalInfo
+		res.render('textview', res.app.locals.renderingOptions)
+	})
+})
+
+module.exports = router
